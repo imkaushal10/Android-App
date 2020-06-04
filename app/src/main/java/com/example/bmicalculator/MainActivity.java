5a22ad118b80f3bb89a98572b4022c276a9e4e5b
imkaushal10 <kb147181@gmail.com>
2020-06-04 19:28:25 +0545

    Bmi Calculator

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText height, weight;
+    TextView result;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        height = (EditText) findViewById(R.id.height);
+        weight = (EditText) findViewById(R.id.weight);
+        btn = (Button) findViewById(R.id.btn);
+        result = (TextView) findViewById(R.id.result);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bmi();
+            }
+        });
+    }
+
+        public void bmi (){
+            float h, w;
+            String bmilabel ="";
+
+            w = Float.parseFloat(weight.getText().toString());
+            h = Float.parseFloat(height.getText().toString());
+
+            float r = (w / (h * h));
+            /*result.setText("BMI"+"="+r);*/
+
+            if (r < 18.5){
+                bmilabel = getString(R.string.underweight);
+            }
+            else if ((r >= 18.5) && (r <= 24.9)) {
+                bmilabel = getString(R.string.healthy_normal_weight);
+            }
+            else if ((r >= 25) && (r <= 29.9)){
+                bmilabel = getString(R.string.overweight);
+            }
+
+            bmilabel = "BMI"+"="+r + "\n" + bmilabel;
+
+            result.setText(bmilabel);
+        }
+    }
+
+
